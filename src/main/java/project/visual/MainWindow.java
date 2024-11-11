04d76cac1c2a52a155b49a6066e86880803c36eb
eliasdlc <exdj0002@ce.pucmm.edu.do>
2024-11-11 15:02:34 -0400

    Started building MainWindow.java

--- src/main/java/project/visual/MainWindow.java
+++ src/main/java/project/visual/MainWindow.java
@@ -3,16 +3,25 @@ package project.visual;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class MainWindow extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("main-window.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+
+        double screenWidth = Screen.getPrimary().getBounds().getWidth() - 50;
+        double screenHeight = Screen.getPrimary().getBounds().getHeight() - 50;
+
+        Scene scene = new Scene(fxmlLoader.load(), screenWidth, screenHeight, javafx.scene.paint.Color.gray(0.9));
         stage.setTitle("Hello!");
+        stage.centerOnScreen();
+        //stage.setResizable(false);
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
